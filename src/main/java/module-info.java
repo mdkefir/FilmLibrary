5929915141f3242e3f5a242368fe8e02d9a75cb9
mdkefir <mykadassa@gmail.com>
2024-04-01 05:16:06 +0800

    Версия с только работающей категории фильмов, парсящего изображения и названия фильма с сайта https://lordfilm.ai/
    НО РАБОТАЕТ!!!

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,8 @@ module com.mdkefir.filmlibrary {
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires org.jsoup;
+    requires java.desktop;
 
     opens com.mdkefir.filmlibrary.controllers to javafx.fxml;
     exports com.mdkefir.filmlibrary;
